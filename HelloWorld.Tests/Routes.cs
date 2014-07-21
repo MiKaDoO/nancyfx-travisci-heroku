f224dfe71d829ad9099e836501cab343b70a68a8
Mickael Metesreau <mickael.metesreau@gmail.com>
2014-07-21 19:43:39 +0200

    Initial commit

---
+++ HelloWorld.Tests/Routes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Nancy;
+using Nancy.Testing;
+using NFluent;
+
+namespace HelloWorld.Tests
+{
+	[TestFixture]
+	public class Routes
+	{
+		[Test]
+		public void Should_return_status_ok_when_route_exists()
+		{
+			// Given
+			var bootstrapper = new DefaultNancyBootstrapper();
+			var browser = new Browser(bootstrapper);
+
+			// When
+			var result = browser.Get("/", with => {
+				with.HttpRequest();
+			});
+
+			// Then
+			Check.That(result.StatusCode).Equals(result.StatusCode);
+		}
+	}
+}
+
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using System.Threading;
+using System.Linq;
+using System.Configuration;
+
+namespace HelloWorld
+{
+	class MainClass
+	{
+		private const string HOST_KEY = "host";
+
+		public static void Main (string[] args)
+		{
+			var uri = ConfigurationManager.AppSettings [HOST_KEY];
+
+			string port = string.Empty;
+			if (args.Length > 0) {
+				int tmp;
+				if (int.TryParse (args [0], out tmp)) {
+					port = tmp.ToString ();
+
+					var host = new NancyHost (new Uri (uri + ":" + port), new Uri("http://0.0.0.0:" + port));
+
+					host.Start (); 
+
+					while(true)
+					{
+						Console.ReadLine();
+					}
+				} 
+			}
+		}
+	}
+}
